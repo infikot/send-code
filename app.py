c67d9b1768b9acc7b7b93213bb4b739ea459a27c
INFIKot <92332233+infikot@users.noreply.github.com>
2024-06-24 19:50:12 +0300

    Ну исправления и добавления
    Добавил пузырёк нейронки и сделал индексирование текста вроде как, есть маленький визуал баг, не критично

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import time
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from pytz import timezone
+from flask import jsonify
 
 tz = timezone('Europe/Moscow')
 
@@ -40,6 +41,7 @@ class Paste(db.Model):
 
 
 @app.route('/')
+@limiter.limit("15 per 3 second", error_message="Spam!")
 def index():
     page = request.args.get('page', 1, type=int)
     per_page = 10
@@ -50,6 +52,8 @@ def index():
 
 
 @app.route('/paste/<int:paste_id>', methods=['GET', 'POST'])
+@limiter.limit("5 per 3 second", error_message="Spam!", methods=['POST'])
+@limiter.limit("15 per 3 second", error_message="Spam!", methods=['GET'])
 def paste(paste_id):
     paste = Paste.query.get_or_404(paste_id)
     if paste.password_hash:
@@ -65,8 +69,8 @@ def paste(paste_id):
 
 
 @app.route('/new', methods=['GET', 'POST'])
-@limiter.limit("1 per 5 second", error_message="Too fast! Try later!")
-@limiter.limit("50 per 1 day", error_message="Spam! U are blocked for 1 day!")
+@limiter.limit("1 per 5 second", error_message="Too fast! Try later!", methods=['POST'])
+@limiter.limit("50 per 1 day", error_message="Spam! U are blocked for 1 day!", methods=['POST'])
 def new_paste():
     if request.method == 'POST':
         title = request.form['title']
@@ -87,8 +91,29 @@ def new_paste():
 
 
 @app.route('/about')
+@limiter.limit("15 per 3 second", error_message="Spam!")
 def about():
     return render_template('about.html')
+
+
+@app.route('/generate_neuro')
+@limiter.limit("1 per 50 second", error_message="Spam!")
+def generate_neuro():
+    # Получаете историю чата из параметров запроса, это тип апи
+    chat_history = request.args.get('history', '')
+
+    try:
+        print(chat_history)
+
+        # тут вы крч указываете запрос и ответ нейронки в resp запишите
+        # хистори не очищается, это её минус думаю вы не глупые, сможете
+        # сделать очистку сами
+
+        resp = "Это ответ нейросети крч!"
+
+        return jsonify({"response": resp})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == '__main__':
